ec9d6144ce1d0f5caa6968577b2b465a9ce9db2c
Gustavo_Lima.37 <129420909+gustavolima37@users.noreply.github.com>
2023-05-22 14:35:26 -0300

    atualizando...em construção

---
+++ JavaScrip/exercicio 3/conversor.js
@@ -0,0 +1,26 @@
+const medida = prompt("Digite um numero em metros, para converter em: \na) milímetro \nb) centímetro \nc) decímetro \nd) decâmetro \ne) hectômetro \nf) quilômetro " )
+
+const medidaNumerica = parseFloat(medida)
+
+switch (medidaNumerica) {
+    case "a":
+        alert(medidaNumerica + " em milimetro é: " + (medidaNumerica / 1000))
+        break
+    case "b":
+        alert(medidaNumerica + " em milimetro é: " + (medmedidaNumericaida / 1000))
+        break 
+    case "c":
+        alert(medidaNumerica + " em milimetro é: " + (medidaNumerica / 1000))
+        break
+    case "d":
+        alert(medidaNumerica + " em milimetro é: " + (medidaNumerica / 1000))
+        break 
+    case "e":
+        alert(medidaNumerica + " em milimetro é: " + (medidaNumerica / 1000))
+        break 
+    case "f":
+        alert(medidaNumerica + " em milimetro é: " + (medidaNumerica / 1000))
+        break 
+    default:
+        alert("Digite um numero valido!")
+}
