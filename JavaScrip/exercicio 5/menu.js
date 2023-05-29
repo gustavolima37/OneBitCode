426b4ba5e7f6520235a23ce7159cbb909e1bd183
Gustavo_Lima.37 <129420909+gustavolima37@users.noreply.github.com>
2023-05-29 12:18:24 -0300

    atualizando...

--- JavaScrip/exercicio 5/menu.js
+++ JavaScrip/exercicio 5/menu.js
@@ -1,36 +1,35 @@
-let opção = "5"
+let opcao = "5"
 
 do {
-    opção = prompt(
-        "Seja bem vindo(a) \n" +
+    opcao = prompt(
+        "Seja bem vindo(a)" +
         "\nEscolha uma das opções abaixo:" +
-        "\n1. Opção Um" + 
-        "\n2. Opção Dois" + 
-        "\n3. Opção Três" + 
-        "\n4. Opção Quatro" + 
-        "\n5. Encerrar"  
-        )
-    
-    switch (opção) {
+        "\n1. opção Um" +
+        "\n2. opção Dois" +
+        "\n3. opção Três" +
+        "\n4. opção Quatro" +
+        "\n5. opção Encerrar" 
+    )
+    switch (opcao) {
         case "1":
-            alert("Você escolheu a opção 1")
+            alert("Você escolheu opção 1")
             break
         case "2":
-            alert("Você escolheu a opção 2")
+            alert("Você escolheu opção 2")
             break
         case "3":
-            alert("Você escolheu a opção 3")
+            alert("Você escolheu opção 3")
             break
         case "4":
-            alert("Você escolheu a opção 4")
+            alert("Você escolheu opção 4")
             break
         case "5":
             alert("Você escolheu encerrar.")
             alert("Encerrando...")
             break
         default:
-            alert("Opção inválida.")
+            alert("Opção errada!")
+            break
     }
-        
-
-} while (opção !== "5")
+  
+} while (opcao !== "5")
